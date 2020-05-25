997a8a3580a0bdc1f45c86f8878983a8ca01b74f
Ryan The <ryan.the.2006@gmail.com>
2020-05-25 11:00:00 +0800

    Enable upload to firestore database

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -3,9 +3,15 @@ import dynamic from "next/dynamic";
 import style from "./style.module.css";
 import sha256 from "crypto-js/sha256";
 
-export default function ScanPage() {
-  const QrReader = dynamic(() => import("react-qr-reader"), { ssr: false });
+import * as firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
 
+// import QrReader from "react-qr-reader"
+const QrReader = dynamic(() => import("react-qr-reader"), { ssr: false });
+
+export default function App() {
+  
   return (
     <div className={style.main}>
       <h1 className={style.header}>Attendance Scanner</h1>
@@ -18,8 +24,11 @@ export default function ScanPage() {
           const message = new Date().getDate();
           const nonce = "sstinc";
           const hashed = sha256(nonce + message).toString();
-          if (result === hashed) {
+          // if (result === hashed) {
+          if (result !== null) {
             console.log("pass");
+            var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithRedirect(provider);
           }
         }}
         className={style.qr}
