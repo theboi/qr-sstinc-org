050744e0ce9244ba3874ca39b4004078edca6a18
Ryan The <ryan.the.2006@gmail.com>
2020-08-15 16:08:01 +0800

    ✨ Update spreadsheet headers to SGT

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,37 +1,45 @@
 export default (req, res) => {
-  res.statusCode = 200
+  res.statusCode = 200;
 
-  const admin = require('firebase-admin');
+  const admin = require("firebase-admin");
 
   const serviceAccount = {
     type: "service_account",
     project_id: "qr-sstinc-org",
     private_key_id: "38367dbeb6166ae7ec8cea36c82478333e0ff95a",
-    private_key: process.env.GCP_SERVICE_ACCOUNT_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.GCP_SERVICE_ACCOUNT_KEY.replace(/\\n/g, "\n"),
     client_email: "qr-sstinc-org@qr-sstinc-org.iam.gserviceaccount.com",
     client_id: "110160196758503350057",
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/qr-sstinc-org%40qr-sstinc-org.iam.gserviceaccount.com"
-  }
+    client_x509_cert_url:
+      "https://www.googleapis.com/robot/v1/metadata/x509/qr-sstinc-org%40qr-sstinc-org.iam.gserviceaccount.com",
+  };
 
-  if (!admin.apps.length) admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
+  if (!admin.apps.length)
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
 
-  admin.firestore().collection('users').get().then(col => {
-    const output = col.docs.map(value => {
-      let data = value.data()
-      const dateInSgt = data.date.toDate().toLocaleString("en-SG", {timeZone: 'Asia/Singapore'})
-      return {
-        timestamp: `${dateInSgt} (SGT)`,
-        email: data.emailAddress,
-        name: data.displayName
-      }
+  admin
+    .firestore()
+    .collection("users")
+    .get()
+    .then((col) => {
+      const output = col.docs.map((value) => {
+        let data = value.data();
+        return {
+          "Timestamp (SGT)": data.date
+            .toDate()
+            .toLocaleString("en-SG", { timeZone: "Asia/Singapore" }),
+          Email: data.emailAddress,
+          Name: data.displayName,
+        };
+      });
+      res.json(output);
     })
-    res.json(output)
-  }).catch((e: Error) => {
-    console.error("ERROR", e)
-  })
-}
+    .catch((e: Error) => {
+      console.error("ERROR", e);
+    });
+};
