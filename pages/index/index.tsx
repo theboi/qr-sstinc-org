ce3dde206f220f23d3b907f8cc1ef5f0ec182efa
Ryan The <ryan.the.2006@gmail.com>
2020-08-17 22:21:47 +0800

    ✨ Add screen to show success or fail more obvious

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -3,6 +3,8 @@ import dynamic from "next/dynamic";
 import style from "./style.module.css";
 import hash from "crypto-js/sha256";
 
+import svgImages from "./svgImages"
+
 import * as firebase from "firebase/app";
 import "firebase/analytics";
 import "firebase/auth";
@@ -11,10 +13,17 @@ import "firebase/firestore";
 // import QrReader from "react-qr-reader"
 const QrReader = dynamic(() => import("react-qr-reader"), { ssr: false });
 
+enum LoginStatus {
+  None,
+  Fail,
+  Success,
+}
+
 export default function App() {
   const [debugMessage, updateDebugMessage] = useState(
     `If you're reading this its too late...`
   );
+  const [loginStatus, updateLoginStatus] = useState(LoginStatus.Success);
 
   useEffect(() => {
     const firebaseConfig = {
@@ -53,31 +62,13 @@ export default function App() {
             date: firebase.firestore.Timestamp.fromDate(new Date()),
           })
           .then((docRef) => {
-            updateDebugMessage(`Success`);
+            updateLoginStatus(LoginStatus.Success);
             console.log(`Successful document written with ID: ${docRef.id}`);
           })
           .catch((error) => {
-            updateDebugMessage(`Error`);
+            updateLoginStatus(LoginStatus.Fail);
             console.error(`Error adding document: ${error}`);
           });
-
-        // firebase
-        //   .database()
-        //   .ref(`users/${user.uid}`)
-        //   .set({
-        //     email: user.email,
-        //     name: user.displayName,
-        //     date: firebase.database.ServerValue.TIMESTAMP,
-        //   })
-        //   .then((res) => {
-        //     console.log(res)
-        //     updateDebugMessage(`Success`);
-        //     console.log(`Success`);
-        //   })
-        //   .catch((err) => {
-        //     updateDebugMessage(`Error: ${err}`);
-        //     console.error(`Error adding document: ${err}`);
-        //   });
       })
       .catch(function (error) {
         console.warn(`${error.message} (Ignore if you have yet to log in)`);
@@ -87,106 +78,123 @@ export default function App() {
   let loadingOverlayRef = useRef(null);
 
   return (
-    <div className={style.main}>
-      <a href="https://sstinc.org" rel="noreferrer noopener" target="_blank">
-        <img
-          src="/assets/sstinc-icon.png"
-          alt="SST Inc Icon"
-          width={100}
-          height={100}
-        />
-      </a>
-      <div className={style.contentDiv}>
-        <h3>SST Inc Attendance Scanner</h3>
-        <p>
-          Kindy scan the given QR code below. A prompt will appear if
-          successful.
-        </p>
-        <QrReader
-          delay={500}
-          onError={(error: Error) => {
-            console.error(`${error}`);
-            if (error.name === "NotAllowedError")
-              return updateDebugMessage(`Error: Please enable your camera`);
-            else if (error.name === "NoVideoInputDevicesError")
-              return updateDebugMessage(
-                `Error: No camera found (Please use Safari if on iOS)`
-              );
-            updateDebugMessage(`Error: ${error.name}`);
-          }}
-          onScan={(result) => {
-            const twoDigitify = (value: number) =>
-              ("0" + value.toString()).slice(-2);
-            const date = new Date();
-            const hashed = hash(
-              `sstinc${twoDigitify(date.getDate())}${twoDigitify(
-                date.getMonth() + 1
-              )}${twoDigitify(date.getFullYear())}`
-            ).toString();
-            console.log(date.getMonth());
-            if (result === hashed) {
-              console.log("pass");
-              var provider = new firebase.auth.GoogleAuthProvider();
-              firebase.auth().signInWithRedirect(provider);
-            }
-          }}
-          className={style.qr}
-        />
-        <p
-          className={style.debug}
-          style={{
-            backgroundColor: (() => {
-              switch (debugMessage) {
-                case "Error":
-                  return "#ff6c2d";
-                case "Success":
-                  return "green";
-                default:
-                  return "transparent";
-              }
-            })(),
-          }}
-        >
-          {debugMessage}
-        </p>
-        <div className={style.loadingOverlay} ref={loadingOverlayRef}>
-          <svg
-            className={style.loadingCircle}
-            width="100"
-            height="100"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="xMidYMid"
-            display="block"
-          >
-            <circle
-              cx="50"
-              cy="50"
-              fill="none"
-              stroke="#ff4a22"
-              strokeWidth="10"
-              r="35"
-              strokeDasharray="164.93361431346415 56.97787143782138"
-              transform="rotate(287.844 50 50)"
-            >
-              <animateTransform
-                attributeName="transform"
-                type="rotate"
-                repeatCount="indefinite"
-                dur="1s"
-                values="0 50 50;360 50 50"
-                keyTimes="0;1"
+    <>
+      <div className={style.main}>
+        <a href="https://sstinc.org" rel="noreferrer noopener" target="_blank">
+          <img
+            src="/assets/sstinc-icon.png"
+            alt="SST Inc Icon"
+            width={100}
+            height={100}
+          />
+        </a>
+        {loginStatus === LoginStatus.Success ||
+        loginStatus === LoginStatus.Fail ? (
+          <>
+            <div className={style.contentDiv}>
+              <h3>{loginStatus === LoginStatus.Success ? "Success" : "Failed"}</h3>
+              {loginStatus === LoginStatus.Success ? svgImages.checkmark : svgImages.cross}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={style.contentDiv}>
+              <h3>SST Inc Attendance Scanner</h3>
+              <p>
+                Kindy scan the given QR code below. A prompt will appear if
+                successful.
+              </p>
+              <QrReader
+                delay={500}
+                onError={(error: Error) => {
+                  console.error(error);
+                  if (error.name === "NotAllowedError")
+                    return updateDebugMessage(
+                      `Error: Please enable your camera`
+                    );
+                  else if (error.name === "NoVideoInputDevicesError")
+                    return updateDebugMessage(
+                      `Error: No camera found (Please use Safari if on iOS)`
+                    );
+                  updateDebugMessage(`Error: ${error.name}`);
+                }}
+                onScan={(result) => {
+                  const twoDigitify = (value: number) =>
+                    ("0" + value.toString()).slice(-2);
+                  const date = new Date();
+                  const hashed = hash(
+                    `sstinc${twoDigitify(date.getDate())}${twoDigitify(
+                      date.getMonth() + 1
+                    )}${twoDigitify(date.getFullYear())}`
+                  ).toString();
+                  console.log(date.getMonth());
+                  if (result === hashed) {
+                    console.log("pass");
+                    var provider = new firebase.auth.GoogleAuthProvider();
+                    firebase.auth().signInWithRedirect(provider);
+                  }
+                }}
+                className={style.qr}
               />
-            </circle>
-          </svg>
-        </div>
+              <p
+                className={style.debug}
+                style={{
+                  backgroundColor: (() => {
+                    switch (debugMessage) {
+                      case "Error":
+                        return "#ff6c2d";
+                      case "Success":
+                        return "green";
+                      default:
+                        return "transparent";
+                    }
+                  })(),
+                }}
+              >
+                {debugMessage}
+              </p>
+              <div className={style.loadingOverlay} ref={loadingOverlayRef}>
+                <svg
+                  className={style.loadingCircle}
+                  width="100"
+                  height="100"
+                  viewBox="0 0 100 100"
+                  preserveAspectRatio="xMidYMid"
+                  display="block"
+                >
+                  <circle
+                    cx="50"
+                    cy="50"
+                    fill="none"
+                    stroke="#ff4a22"
+                    strokeWidth="10"
+                    r="35"
+                    strokeDasharray="164.93361431346415 56.97787143782138"
+                    transform="rotate(287.844 50 50)"
+                  >
+                    <animateTransform
+                      attributeName="transform"
+                      type="rotate"
+                      repeatCount="indefinite"
+                      dur="1s"
+                      values="0 50 50;360 50 50"
+                      keyTimes="0;1"
+                    />
+                  </circle>
+                </svg>
+              </div>
+            </div>
+          </>
+        )}
+
+        <p className={style.about}>
+          Created by{" "}
+          <a className={style.link} href="https://ryanthe.com">
+            Ryan The
+          </a>{" "}
+          from SST Inc | 2020 | v1.0
+        </p>
       </div>
-      <p className={style.about}>
-        Created by{" "}
-        <a className={style.link} href="https://ryanthe.com">
-          Ryan The
-        </a>{" "}
-        from SST Inc | 2020 | v1.0
-      </p>
-    </div>
+    </>
   );
 }
