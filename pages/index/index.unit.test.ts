f9f7252f3350d2d70ec91f0f32c9be42be05c2dc
Ryan The <ryan.the.2006@gmail.com>
2020-09-07 18:20:33 +0800

    🚚 Tidy files into folders

--- pages/index/index.unit.test.ts
+++ pages/index/index.unit.test.ts
@@ -1,9 +1,11 @@
-import {getTruncatedDate, getKeyString} from "./index"
+import { getTruncatedDate, getKeyString } from "./index";
 
-test('getTruncatedDate', () => {
-  expect(getTruncatedDate(139)).toBe(39)
-})
+test("getTruncatedDate", () => {
+  expect(getTruncatedDate(139)).toBe(39);
+});
 
-test('getKeyString', () => {
-  expect(getKeyString('noice', new Date('December 17, 1995 03:24:00'))).toBe('noice171295')
-})
+test("getKeyString", () => {
+  expect(getKeyString("noice", new Date("December 17, 1995 03:24:00"))).toBe(
+    "noice171295"
+  );
+});
